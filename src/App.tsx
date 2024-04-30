0a7d63ed6586a6c988830c9e19a8073ae2f13256
102sae <102chimee@gmail.com>
2024-04-30 17:21:04 +0900

    style : global css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Router from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
+import "./style/global.css";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <div className="container">
+      <div className="content">
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
