3e94bd0f5ef314b8f95e0fa51ece451a93f8cac6
102sae <102chimee@gmail.com>
2024-04-30 18:26:12 +0900

    add : 거래 리스트 컴포넌트

---
+++ src/components/trade/SaleListItem/SalesListItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./styles.css";
+import building from "../../../assets/icons/building.png";
+
+type SalesListItemProps = {
+  location: string;
+  name: string;
+  info: string;
+  profit: number;
+};
+
+const SalesListItem: React.FC<SalesListItemProps> = ({
+  location,
+  name,
+  info,
+  profit,
+}) => {
+  return (
+    <div className="SalesListItem">
+      <div className="col icon">
+        <img src={building} alt="building" />
+      </div>
+      <div className="col">
+        <div className="row location">{location}</div>
+        <div className="row name">{name}</div>
+        <div className="row info">{info}</div>
+      </div>
+      <div className="col">
+        <div className="profit_badge">
+          {profit > 0 ? (
+            <div className="positive">+{profit}%</div>
+          ) : (
+            <div className="negative">{profit}%</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SalesListItem;
--- src/pages/TragePage/TradePage.tsx
+++ src/pages/TragePage/TradePage.tsx
@@ -1,11 +1,40 @@
-import React from "react";
 import { Header } from "../../components";
 import arrow from "../../assets/icons/arrow.svg";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../../components/trade/SearchBar/SearchBar";
+import SalesListItem from "../../components/trade/SaleListItem/SalesListItem";
+import "./styles.css";
 
 const TradePage = () => {
   const navigate = useNavigate();
+
+  const saleLists = [
+    {
+      location: "서울 성동구",
+      name: "성수 코오롱 타워",
+      info: "서울 더블 역세권",
+      profit: 3.5,
+    },
+    {
+      location: "서울 성동구",
+      name: "성수 코오롱 타워",
+      info: "서울 더블 역세권",
+      profit: -3.5,
+    },
+    {
+      location: "서울 성동구",
+      name: "성수 코오롱 타워",
+      info: "서울 더블 역세권",
+      profit: 3.5,
+    },
+    {
+      location: "서울 성동구",
+      name: "성수 코오롱 타워",
+      info: "서울 더블 역세권",
+      profit: -3.5,
+    },
+  ];
+
   return (
     <div>
       <Header
@@ -15,6 +44,18 @@ const TradePage = () => {
       ></Header>
       <h3>거래 중인 건물과 토지를 검색해 보세요</h3>
       <SearchBar placeholder="성수, 코오롱 타워 " />
+
+      <div className="salesList">
+        {saleLists.map((saleList, index) => (
+          <SalesListItem
+            key={index}
+            location={saleList.location}
+            name={saleList.name}
+            info={saleList.info}
+            profit={saleList.profit}
+          />
+        ))}
+      </div>
     </div>
   );
 };
