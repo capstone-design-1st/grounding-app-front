9e83a2f2475b5f92782888c17488a7f8f882b5b7
102sae <102chimee@gmail.com>
2024-05-17 11:18:24 +0900

    add : 리액트쿼리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Router from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./style/global.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="container">
-      <div className="content">
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <div className="content">
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
