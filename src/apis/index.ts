ab06c825aff3ef31bf9d8b65e770ab4286ae54aa
102sae <102chimee@gmail.com>
2024-05-23 13:26:25 +0900

    add : 헤더에 토큰 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -11,3 +11,20 @@ export const instance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// 요청 인터셉터 추가
+instance.interceptors.request.use(
+  (config) => {
+    // 로컬 스토리지에서 토큰 가져오기
+    const token = localStorage.getItem("token");
+    if (token) {
+      // 토큰이 있으면 Authorization 헤더에 추가
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // 요청 에러 처리
+    return Promise.reject(error);
+  }
+);
