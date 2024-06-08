91c207eb15a3ba3d63aca9e94d1eaccf58818c48
102sae <102chimee@gmail.com>
2024-06-08 19:15:30 +0900

    add : 입출금 api 연결

--- src/pages/DepositPage/DepositPage.tsx
+++ src/pages/DepositPage/DepositPage.tsx
@@ -1,52 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import arrow from "../../assets/icons/arrow.svg";
 import "./styles.css";
 import { Button, Header } from "../../components";
+import { useMutation } from "react-query";
+import { postDeposit } from "../../apis/Mypage";
 
 const DepositPage: React.FC = () => {
   const navigate = useNavigate();
 
+  const [amount, setAmount] = useState<number>(0);
+
+  const depositMutation = useMutation(postDeposit, {
+    onSuccess: () => {
+      alert("입금이 성공적으로 처리되었습니다.");
+    },
+    onError: (error: any) => {
+      alert(`입금 중 오류가 발생했습니다: ${error.message}`);
+    },
+  });
+
+  const handleDeposit = () => {
+    depositMutation.mutate({ deposit_amount: amount });
+  };
+
   return (
-    <div className="depositPage">
+    <div>
       <Header
         leftContent={
           <img src={arrow} alt="Back" onClick={() => navigate(-1)} />
         }
         centerContent={<strong>입금하기</strong>}
       />
-
-      <div className="content">
-        <div className="row">
-          <div className="label">실명 계좌</div>
-          <div className="value">하나은행 1001001*****</div>
+      <div className="depositPage">
+        <div className="content">
+          <div className="row">
+            <div className="label">실명 계좌</div>
+            <div className="value">하나은행 1001001*****</div>
+          </div>
+          <div className="row">
+            <div className="label">입금 금액(원)</div>
+          </div>
+          <input
+            type="text"
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+          <div className="noteTitle">입금 시 유의사항</div>
+          <ul className="notes">
+            <li>
+              입금 실행시, 연계된 실명확인 계좌에서 신청한 금액만큼 자동
+              이체됩니다.
+            </li>
+            <li>
+              입금가능금액은 연계된 실명확인 계좌의 출금가능금액과 동일하며,
+              연계 계좌의 이체한도 범위 등을 벗어나는 경우 입금이 정상적으로
+              진행되지 않을 수도 있습니다.
+            </li>
+          </ul>
         </div>
-        <div className="row">
-          <div className="label">입금 금액(원)</div>
-        </div>
-        <input type="text" />
-        <div className="noteTitle">입금 시 유의사항</div>
-        <ul className="notes">
-          <li>
-            입금 실행시, 연계된 실명확인 계좌에서 신청한 금액만큼 자동
-            이체됩니다.
-          </li>
-          <li>
-            입금가능금액은 연계된 실명확인 계좌의 출금가능금액과 동일하며, 연계
-            계좌의 이체한도 범위 등을 벗어나는 경우 입금이 정상적으로 진행되지
-            않을 수도 있습니다.
-          </li>
-        </ul>
       </div>
-      <Button
-        text="입금하기"
-        color="#fff"
-        background="var(--main)"
-        padding="15px 10px"
-        onClick={() => {
-          console.log("입금하기");
-        }}
-      />
+      <div className="buttonWrapper">
+        <Button
+          text="입금하기"
+          color="#fff"
+          background="var(--main)"
+          padding="15px 10px"
+          onClick={handleDeposit}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/WithdrawPage/WithdrawPage.tsx
+++ src/pages/WithdrawPage/WithdrawPage.tsx
@@ -1,52 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import arrow from "../../assets/icons/arrow.svg";
 import "./styles.css";
 import { Button, Header } from "../../components";
+import { useMutation } from "react-query";
+import { postWithdraw } from "../../apis/Mypage";
 
 const WithdrawPage: React.FC = () => {
   const navigate = useNavigate();
 
+  const [amount, setAmount] = useState<number>(0);
+
+  const depositMutation = useMutation(postWithdraw, {
+    onSuccess: () => {
+      alert("출금이 성공적으로 처리되었습니다.");
+    },
+    onError: (error: any) => {
+      alert(`출금 중 오류가 발생했습니다: ${error.message}`);
+    },
+  });
+
+  const handleDeposit = () => {
+    depositMutation.mutate({ withdrawal_amount: amount + 1000 });
+  };
+
   return (
-    <div className="withdrawPage">
+    <div>
       <Header
         leftContent={
           <img src={arrow} alt="Back" onClick={() => navigate(-1)} />
         }
         centerContent={<strong>출금하기</strong>}
       />
-
-      <div className="content">
-        <div className="row">
-          <div className="label">실명 계좌</div>
-          <div className="value">신한은행 110-123-456789</div>
-        </div>
-        <div className="row">
-          <div className="label">출금 가능</div>
-          <div className="value">123,123원</div>
-        </div>
-        <div className="inputWrapper">
-          <label className="label">출금 금액(원)</label>
-          <input type="text" />
-        </div>
-        <div className="row note">
-          <div className="label">수수료(부가세 포함)</div>
-          <div className="value">1000원</div>
-        </div>
-        <div className="row note">
-          <div className="label">총출금(수수료 포함)</div>
-          <div className="value">1000원</div>
+      <div className="withdrawPage">
+        <div className="content">
+          <div className="row">
+            <div className="label">실명 계좌</div>
+            <div className="value">하나은행 1001001*****</div>
+          </div>
+          <div className="inputWrapper">
+            <label className="label">출금 금액(원)</label>
+            <input
+              type="text"
+              onChange={(e) => setAmount(Number(e.target.value))}
+            />
+          </div>
+          <div className="row note">
+            <div className="label">수수료(부가세 포함)</div>
+            <div className="value">1000원</div>
+          </div>
+          <div className="row note">
+            <div className="label">총출금(수수료 포함)</div>
+            <div className="value">{amount + 1000}원</div>
+          </div>
         </div>
       </div>
-      <Button
-        text="출금하기"
-        color="#fff"
-        background="var(--main)"
-        padding="15px 10px"
-        onClick={() => {
-          console.log("출금하기");
-        }}
-      />
+      <div className="buttonWrapper">
+        <Button
+          text="출금하기"
+          color="#fff"
+          background="var(--main)"
+          padding="15px 10px"
+          onClick={handleDeposit}
+        />
+      </div>
     </div>
   );
 };
