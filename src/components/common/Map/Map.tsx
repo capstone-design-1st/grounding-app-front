5bd70fff2690f19397ec6131689c17f6b568a6de
102sae <102chimee@gmail.com>
2024-06-11 01:03:59 +0900

    fix : kakao map 보이게 수정

--- src/components/common/Map/Map.tsx
+++ src/components/common/Map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 declare global {
   interface Window {
@@ -11,57 +11,44 @@ interface MapProps {
 }
 
 const Map: React.FC<MapProps> = ({ address }) => {
-  const [lat, setLat] = useState<number | null>(null);
-  const [lng, setLng] = useState<number | null>(null);
+  const [coordinates, setCoordinates] = useState(null); // 좌표를 저장할 상태
+  const mapContainer = useRef<HTMLDivElement>(null); // 지도를 표시할 div에 대한 참조
 
   useEffect(() => {
-    const { kakao } = window;
-    //모바일에서 맵 보이게 하기
-    kakao.maps.disableHD();
+    if (window.kakao && mapContainer.current) {
+      const { kakao } = window;
 
-    const geocoder = new kakao.maps.services.Geocoder();
-    geocoder.addressSearch(address, function (result: any, status: any) {
-      if (status === kakao.maps.services.Status.OK) {
-        const coordinates = new kakao.maps.LatLng(result[0].y, result[0].x);
-        setLat(result[0].y);
-        setLng(result[0].x);
+      const geocoder = new kakao.maps.services.Geocoder();
+      geocoder.addressSearch(address, function (result: any, status: any) {
+        if (status === kakao.maps.services.Status.OK) {
+          const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+          setCoordinates(coords); // 검색된 좌표를 상태에 저장
+        }
+      });
+    }
+  }, [address]); // 주소가 변경될 때마다 이 useEffect가 실행됩니다.
 
-        const container = document.getElementById("map");
-        const options = {
-          center: coordinates,
-          level: 3,
-        };
+  useEffect(() => {
+    if (coordinates && mapContainer.current) {
+      const { kakao } = window;
+      const options = {
+        center: coordinates,
+        level: 3,
+      };
+      //모바일에서 지도 해상도 낮추기
+      kakao.maps.disableHD();
+      const map = new kakao.maps.Map(mapContainer.current, options); // 좌표가 준비되면 지도 생성
+      new kakao.maps.Marker({
+        map: map,
+        position: coordinates, // 마커 생성
+      });
+    }
+  }, [coordinates]); // 좌표가 설정되면 이 useEffect가 실행되어 지도를 로드
 
-        const map = new kakao.maps.Map(container, options);
-
-        // 마커 생성
-        new kakao.maps.Marker({
-          map: map,
-          position: coordinates,
-        });
-      }
-    });
-  }, [address]);
-
-  return lat && lng ? (
-    <a
-      href={`https://map.kakao.com/link/map/${encodeURIComponent(
-        address
-      )},${lat},${lng}`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div
-        id="map"
-        style={{
-          width: "100%",
-          height: "200px",
-        }}
-      />
-    </a>
-  ) : (
+  return (
     <div
       id="map"
+      ref={mapContainer}
       style={{
         width: "100%",
         height: "200px",
