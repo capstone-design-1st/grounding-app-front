090208b0d63a0e241f9cd8132a5093e032a17479
102sae <102chimee@gmail.com>
2024-06-12 15:36:43 +0900

    fix : 빌드 에러 수정

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,24 +1,30 @@
-import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
-import Slider from 'react-slick';
-import './styles.css';
-import '../../style/slick.css';
-import '../../style/slick-theme.css';
-import { fetchListingsByVolume, getOnGoingList } from '../../apis/Home';
-import { getMyAccountInventory } from '../../apis/Mypage';
-import { getLikeList } from '../../apis/Likes';
-import { Header, Navbar, AssetListItem, AssetRankingItem, SkeletonLoader } from '../../components';
-import { calculateTotalReturn } from '../../util/calculateTotal';
-import mainImg from '../../assets/imgs/main.png';
-import dotsIcon from '../../assets/icons/dots.svg';
-import banner1 from '../../assets/imgs/banner1.png';
-import banner2 from '../../assets/imgs/banner2.png';
-import logo from '../../assets/imgs/header-logo.png';
-import { formatNumberWithCommas } from '../../util/formatNumber';
-import { getToken } from '../../util/token';
-import { createClient } from '../../util/xrpl/wallet';
-import { useXrplClientStore } from '../../store/xrplStore';
-import { useEffect } from 'react';
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "./styles.css";
+import "../../style/slick.css";
+import "../../style/slick-theme.css";
+import { fetchListingsByVolume, getOnGoingList } from "../../apis/Home";
+import { getMyAccountInventory } from "../../apis/Mypage";
+import { getLikeList } from "../../apis/Likes";
+import {
+  Header,
+  Navbar,
+  AssetListItem,
+  AssetRankingItem,
+  SkeletonLoader,
+} from "../../components";
+import { calculateTotalReturn } from "../../util/calculateTotal";
+import mainImg from "../../assets/imgs/main.png";
+import dotsIcon from "../../assets/icons/dots.svg";
+import banner1 from "../../assets/imgs/banner1.png";
+import banner2 from "../../assets/imgs/banner2.png";
+import logo from "../../assets/imgs/header-logo.png";
+import { formatNumberWithCommas } from "../../util/formatNumber";
+import { getToken } from "../../util/token";
+import { createClient } from "../../util/xrpl/wallet";
+import { useXrplClientStore } from "../../store/xrplStore";
+import { useEffect } from "react";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -26,17 +32,21 @@ const HomePage = () => {
   const { xrplClient, setXrplClient } = useXrplClientStore();
 
   //모집 중 매물 리스트 조회
-  const { data: listings } = useQuery('listings', () => getOnGoingList());
+  const { data: listings } = useQuery("listings", () => getOnGoingList());
 
   const listingsCount = listings?.content.length;
 
   //거래량이 많은 매물 리스트 조회
-  const { data: listingsVolume } = useQuery('listingsVolume', () => fetchListingsByVolume());
+  const { data: listingsVolume } = useQuery("listingsVolume", () =>
+    fetchListingsByVolume()
+  );
 
   //보유 자산 조회
-  const { data: assetHome } = useQuery('assetHome', () => getMyAccountInventory());
+  const { data: assetHome } = useQuery("assetHome", () =>
+    getMyAccountInventory()
+  );
 
-  const { data: likesList } = useQuery('likesList', () => getLikeList());
+  const { data: likesList } = useQuery("likesList", () => getLikeList());
 
   /*슬라이더 세팅 */
   const settings = {
@@ -64,33 +74,47 @@ const HomePage = () => {
         const client = await createClient();
         setXrplClient(client);
 
-        console.log('client', client);
+        console.log("client", client);
       })();
     }
-  }, [setXrplClient]);
+  }, [setXrplClient, xrplClient]);
 
   return (
     <div>
       <Header
-        rightContent={<img src={dotsIcon} alt="dots" style={{ width: '18px', height: '18px' }} />}
-        leftContent={<img src={logo} alt="logo" style={{ width: '57px', height: '22px' }} />}
+        rightContent={
+          <img
+            src={dotsIcon}
+            alt="dots"
+            style={{ width: "18px", height: "18px" }}
+          />
+        }
+        leftContent={
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "57px", height: "22px" }}
+          />
+        }
       />
       <Slider {...bannerSettings}>
         <div>
-          <img src={banner1} alt="Banner 1" style={{ width: '100%' }} />
+          <img src={banner1} alt="Banner 1" style={{ width: "100%" }} />
         </div>
         <div>
-          <img src={banner2} alt="Banner 2" style={{ width: '100%' }} />
+          <img src={banner2} alt="Banner 2" style={{ width: "100%" }} />
         </div>
       </Slider>
       <div className="titleWrapper">
         {listings && listingsCount ? (
           <div>
             <div className="title">모집 중인 매물</div>
-            <div className="subTitle">{listingsCount}개의 매물이 투자를 받고 있어요.</div>
+            <div className="subTitle">
+              {listingsCount}개의 매물이 투자를 받고 있어요.
+            </div>
           </div>
         ) : (
-          <div className="title" style={{ marginBottom: '10px' }}>
+          <div className="title" style={{ marginBottom: "10px" }}>
             <SkeletonLoader width="100%" height="20px" />
           </div>
         )}
@@ -105,7 +129,14 @@ const HomePage = () => {
                   key={listing.property_id}
                   onClick={() => navigate(`/recruit/${listing.property_id}`)}
                 >
-                  <img src={listing.thumbnail_url === null ? mainImg : listing.thumbnail_url} alt={`main ${index}`} />
+                  <img
+                    src={
+                      listing.thumbnail_url === null
+                        ? mainImg
+                        : listing.thumbnail_url
+                    }
+                    alt={`main ${index}`}
+                  />
                   <div className="sliderTitle">{listing.name}</div>
                   <div className="sliderSubTitle">{listing.oneline}</div>
                 </div>
@@ -118,7 +149,14 @@ const HomePage = () => {
                 key={listing.property_id}
                 onClick={() => navigate(`/recruit/${listing.property_id}`)}
               >
-                <img src={listing.thumbnail_url === null ? mainImg : listing.thumbnail_url} alt={`main ${index}`} />
+                <img
+                  src={
+                    listing.thumbnail_url === null
+                      ? mainImg
+                      : listing.thumbnail_url
+                  }
+                  alt={`main ${index}`}
+                />
                 <div className="sliderTitle">{listing.name}</div>
                 <div className="sliderSubTitle">{listing.oneline}</div>
               </div>
@@ -128,9 +166,9 @@ const HomePage = () => {
           <div
             className="subTitle"
             style={{
-              width: '100%',
-              padding: '0 20px',
-              boxSizing: 'border-box',
+              width: "100%",
+              padding: "0 20px",
+              boxSizing: "border-box",
             }}
           >
             <SkeletonLoader width="100%" height="280px" />
@@ -149,20 +187,32 @@ const HomePage = () => {
                   <div className="home assetHeader">
                     <div className="assetTitle">내 자산</div>
                     {totalEarningRatio >= 0 ? (
-                      <div className="overallChange" style={{ color: 'var(--red)' }}>
+                      <div
+                        className="overallChange"
+                        style={{ color: "var(--red)" }}
+                      >
                         +{totalEarningRatio.toFixed(2)}%
                       </div>
                     ) : (
-                      <div className="overallChange" style={{ color: 'var(--blue)' }}>
+                      <div
+                        className="overallChange"
+                        style={{ color: "var(--blue)" }}
+                      >
                         {totalEarningRatio.toFixed(2)}%
                       </div>
                     )}
-                    <div className="viewMore" onClick={() => navigate('/mypage')}>
+                    <div
+                      className="viewMore"
+                      onClick={() => navigate("/mypage")}
+                    >
                       자세히 보기
                     </div>
                   </div>
                   {assetHome?.map((asset: any) => (
-                    <div key={asset.property_id} onClick={() => navigate(`/trade/${asset.property_id}`)}>
+                    <div
+                      key={asset.property_id}
+                      onClick={() => navigate(`/trade/${asset.property_id}`)}
+                    >
                       <AssetListItem
                         isMyAsset={true}
                         assetType={asset.type}
@@ -183,7 +233,10 @@ const HomePage = () => {
               <div className="home assetHeader">
                 <div className="assetTitle">내 자산</div>
               </div>
-              <div className="assetTitle" style={{ padding: '10px 20px', textAlign: 'center' }}>
+              <div
+                className="assetTitle"
+                style={{ padding: "10px 20px", textAlign: "center" }}
+              >
                 {getToken() ? (
                   <div>보유하고 있는 자산이 없습니다.</div>
                 ) : (
@@ -197,16 +250,16 @@ const HomePage = () => {
         ) : (
           <div
             style={{
-              width: '100%',
-              padding: '0 20px',
-              marginTop: '10px',
-              boxSizing: 'border-box',
+              width: "100%",
+              padding: "0 20px",
+              marginTop: "10px",
+              boxSizing: "border-box",
             }}
           >
-            <div className="assetTitle" style={{ marginTop: '10px' }}>
+            <div className="assetTitle" style={{ marginTop: "10px" }}>
               <SkeletonLoader width="100%" height="20px" />
             </div>
-            <div style={{ width: '100%', marginTop: '10px' }}>
+            <div style={{ width: "100%", marginTop: "10px" }}>
               <SkeletonLoader width="100%" height="80px" />
             </div>
           </div>
@@ -234,9 +287,9 @@ const HomePage = () => {
       ) : (
         <div className="titleWrapper">
           <div className="title">내가 찜한 매물</div>
-          <div style={{ textAlign: 'center', padding: '20px' }}>
+          <div style={{ textAlign: "center", padding: "20px" }}>
             {getToken() ? (
-              <div>보유하고 있는 자산이 없습니다.</div>
+              <div>찜한 매물이 없습니다</div>
             ) : (
               <div>
                 <div>로그인 후 관심있는 매물에 찜을 해보세요!</div>
@@ -251,7 +304,10 @@ const HomePage = () => {
           <div>
             <div className="title">거래량이 많은 매물</div>
             {listingsVolume?.content?.map((listing: any, index: number) => (
-              <div key={listing.id} onClick={() => navigate(`/trade/${listing.id}`)}>
+              <div
+                key={listing.id}
+                onClick={() => navigate(`/trade/${listing.id}`)}
+              >
                 <AssetRankingItem
                   rank={index + 1}
                   assetType={listing.type}
@@ -265,10 +321,10 @@ const HomePage = () => {
           </div>
         ) : (
           <div>
-            <div className="title" style={{ marginBottom: '10px' }}>
+            <div className="title" style={{ marginBottom: "10px" }}>
               <SkeletonLoader width="100%" height="20px" />
             </div>
-            <div style={{ width: '100%', marginTop: '10px' }}>
+            <div style={{ width: "100%", marginTop: "10px" }}>
               <SkeletonLoader width="100%" height="80px" />
             </div>
           </div>
